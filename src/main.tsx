5d6dcfeec188d8c057c7c1f71ae2d7472c52bac8
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2026-08-09 09:52:42 +0530

    added live analytics and tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+// Initialize PostHog (Make sure to add these to your .env file)
+posthog.init(import.meta.env.VITE_POSTHOG_KEY || 'phc_placeholder', {
+  api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com',
+  person_profiles: 'always', // records all users (anonymous + identified)
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
